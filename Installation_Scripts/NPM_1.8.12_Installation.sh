00cc74c6d8f4f30a18bdd978e8efbb31843c3d74
anandgit12 <42269935+anandgit12@users.noreply.github.com>
2019-01-09 19:22:56 +0530

    Update NPM_1.8.12_Installation.sh

--- Installation_Scripts/NPM_1.8.12_Installation.sh
+++ Installation_Scripts/NPM_1.8.12_Installation.sh
@@ -16,6 +16,8 @@ echo "################################################################"
 
 sudo yum update -y
 
+yum install wget git unzip zip python php java-1.8.0-openjdk-devel -y
+
 sudo cd /opt
 
 mkdir -p /opt/node-1.8.12
@@ -31,3 +33,8 @@ ln -s /opt/node-1.8.12/node-v8.12.0-linux-x64/bin/node /bin/node
 ln -s /opt/node-1.8.12/node-v8.12.0-linux-x64/bin/npm /bin/npm
 
 sudo npm install -g node-dev node-gyp 
+
+sudo npm install forever -g
+
+ln -s /opt/node-1.8.12/node-v8.12.0-linux-x64/bin/forever /bin/forever
+
